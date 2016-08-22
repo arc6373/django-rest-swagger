de9e682278e2c6f4c6d5e0f68671e773b335a6db
arc6373 <arc6373@rit.edu>
2016-08-22 11:18:10 -0400

    Testing

--- rest_framework_swagger/docgenerator.py
+++ rest_framework_swagger/docgenerator.py
@@ -52,8 +52,10 @@ class DocumentationGenerator(object):
             disabled = SWAGGER_SETTINGS.get('disabled_methods')
             #Gets the operations that are available ie POST or GET
             operations = self.get_operations(api, apis)
+            print disabled
             for operation in operations:
                 #If the method is in the disabled methods list it will remove it
+                print operation['method']
                 if operation['method'] in disabled:
                     operations.remove(operation)
 
