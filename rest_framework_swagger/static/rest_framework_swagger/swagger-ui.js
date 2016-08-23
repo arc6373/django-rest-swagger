6976af967ac4de445aff9e48c58474bc40a156b9
arc6373 <arc6373@rit.edu>
2016-08-23 13:40:49 -0400

    Adding new data types

--- rest_framework_swagger/static/rest_framework_swagger/swagger-ui.js
+++ rest_framework_swagger/static/rest_framework_swagger/swagger-ui.js
@@ -572,7 +572,7 @@ function program10(depth0,data) {
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "' placeholder='' type='text' value='";
+    + "' placeholder='10' type='text' value='";
   if (stack1 = helpers.defaultValue) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.defaultValue; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
@@ -587,7 +587,7 @@ function program12(depth0,data) {
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "' placeholder='Test' type='text' value=''/>\n		";
+    + "' placeholder='12' type='text' value=''/>\n		";
   return buffer;
   }
 
