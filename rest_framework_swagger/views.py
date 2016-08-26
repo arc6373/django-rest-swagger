1c9ca8272781a0504d05d98cdf7ea1700c765985
arc6373 <arc6373@rit.edu>
2016-08-26 13:05:52 -0400

    Adding Top level only filter support 3

--- rest_framework_swagger/views.py
+++ rest_framework_swagger/views.py
@@ -186,7 +186,6 @@ class SwaggerApiView(APIDocView):
         split_url = authorized_apis_list[0]['path'].split('/')
         for piece in split_url:
             if piece in top_level_only:
-                print authorized_apis_list[0]
-                # return authorized_apis_list[0]
+                return [authorized_apis_list[0]]
 
         return authorized_apis_list
